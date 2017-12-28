b3778e53ef4b8e824d766ea8f1bcf41045a86692
xabertum <mohatar@gmail.com>
2017-12-28 14:50:26 +0100

    jQuery test

---
+++ js/.ContactJs.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+
+$(document).ready(function () {
+
+	$("body").click(function () {
+
+		if (this.click()) {
+
+			$('.modalDialog', window.parent.document).css({
+				'opacity': '0',
+				'pointer-events': 'none'
+			});
+		} else if ($(".contact").click()) {
+
+			$('.modalDialog', window.parent.document).css({
+				'opacity': '1',
+				'pointer-events': 'auto'
+			});
+
+
+		}
+
+	});
+
+
+});
