bf6a3aed789819ba51d69794e29045f8f9acc611
xabertum <mohatar@gmail.com>
2017-12-28 13:46:02 +0100

    Minor changes

--- js/ContactJs.js
+++ js/ContactJs.js
@@ -1,22 +1,24 @@
 // JavaScript Document
 
-$(document).ready(function (){
-	
-	$("body").click(function(){
-	
-	if (this.click()) {
-		
-	$('.modalDialog', window.parent.document).css({'opacity': '0', 'pointer-events': 'none'});
-	}
+$(document).ready(function () {
 
- 	
-	
-	else if ($(".contact").click()) {
-	
-	$('.modalDialog', window.parent.document).css({'opacity': '1', 'pointer-events': 'auto'});
-	
+	$("body").click(function () {
 
- 	}
+		if (this.click()) {
+
+			$('.modalDialog', window.parent.document).css({
+				'opacity': '0',
+				'pointer-events': 'none'
+			});
+		} else if ($(".contact").click()) {
+
+			$('.modalDialog', window.parent.document).css({
+				'opacity': '1',
+				'pointer-events': 'auto'
+			});
+
+
+		}
 
 	});
 
